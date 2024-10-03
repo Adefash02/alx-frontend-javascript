3c820671091d3318a168d85f33480c9652cce977
Adefash02 <adelekeabdulmalikae@gmail.com>
2024-10-03 19:36:34 +0000

    o-classes

--- 0x02-ES6_classes/0-classroom.js
+++ 0x02-ES6_classes/0-classroom.js
@@ -2,15 +2,5 @@ export default class ClassRoom {
   constructor(maxStudentsSize) {
     this._maxStudentsSize = maxStudentsSize;
   }
-
-  // Getter method for maxStudentsSize
-  get maxStudentsSize() {
-    return this._maxStudentsSize;
-  }
-
-  // Setter method for maxStudentsSize
-  set maxStudentsSize(value) {
-    this._maxStudentsSize = value;
-  }
 }
 
